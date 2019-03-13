d0c551f99932f006ea7a788db9e1c1e83ad912cf
Soojung Hong <hong.soojung@gmail.com>
2019-03-13 14:43:17 +0100

    Create Time_Series_Analysis_using_LSTM.py

---
+++ Time_Series_Analysis_using_LSTM.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# load data
+apple_training_complete = pd.read_csv('C:/Users/shong/PycharmProjects/keras_exercise/data/AAPL.csv')
+print('apple_training_complete : \n', apple_training_complete)
+apple_training_processed = apple_training_complete.iloc[:, 1:2].values  # only column in index 1 which is 'Open' column
+print('apple_training_processed : \n', apple_training_processed)
+
+
+# data normalization
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+apple_training_scaled = scaler.fit_transform(apple_training_processed)
+print('apple_training_scaled : \n', apple_training_scaled)
+
+
+# feature set
+features_set = []
+labels = []
+for i in range(60, 1260):  # to predict the time T's stock price, we use previous 60 days' value
+      features_set.append(apple_training_scaled[i-60:i, 0])  # adding one array, this one array contains 60 values (0th ~ 59th value), it is ndarray type
+      labels.append(apple_training_scaled[i, 0]) # adding a label, which is 60th apple open stock value is label, it is float64 type
+
+features_set, labels = np.array(features_set), np.array(labels)  # features_set is array of array
+#print('feature_set shape :', features_set.shape)  # (1200, 60)
+#print('labels : ', labels.shape)  # (1200,)
+features_set = np.reshape(features_set, (features_set.shape[0], features_set.shape[1], 1))  # features_set.shape[0] is 1200, features_set.shape[1] is 60
+print('reshaped feature_set shape : \n', features_set.shape)  # shape is three dimensional (1200, 60, 1)
+
+print('features_set.shape[1] : \n', features_set.shape[1])
+
+# training LSTM
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(features_set.shape[1], 1)))  # the number of neurons is 50, return_sequence is true since we will add more layers to the model
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=1))  # the number of neurons in the dense layer will be set to 1 since we want to predict a single value in the output
+
+
+# model compilation - we need to compile out LSTM before we train the model on training data
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+
+# algorithm training
+model.fit(features_set, labels, epochs=100, batch_size=32)
+
+# testing our LSTM
+apple_testing_complete = pd.read_csv('C:/Users/shong/PycharmProjects/keras_exercise/data/apple_testing.csv')
+apple_testing_processed = apple_testing_complete.iloc[:, 1:2].values
+
+# convert test data to right format
+apple_total = pd.concat((apple_training_complete['Open'], apple_testing_complete['Open']), axis=0)
+test_inputs = apple_total[len(apple_total) - len(apple_testing_complete)-60:].values
+
+
+# ToDo : Converting Test Data to Right Format 
+#  https://stackabuse.com/time-series-analysis-with-lstm-using-pythons-keras-library/
